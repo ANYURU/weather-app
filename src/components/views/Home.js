d7935c19417a07844065224f30315736a14f6b19
anyuru <davidderrickanyuru@gmail.com>
2021-10-26 17:46:19 +0300

    Rendered details, hero components. Added some packages

---
+++ src/components/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../partials/Hero'
+import Details from '../partials/Details'
+
+function Home() {
+    return (
+        <>
+            <Hero />
+            <Details />
+        </>
+    )
+}
+
+export default Home
